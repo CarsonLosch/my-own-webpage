b2b75c00e85142afefab4e3386990d7549433087
CarsonLosch <95552371+CarsonLosch@users.noreply.github.com>
2022-01-29 14:40:28 -0800

    added contact info links and css styling

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content addedd by JavaScript";
+h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
